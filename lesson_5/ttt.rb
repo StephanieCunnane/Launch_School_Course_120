45b68fbaee2bc6db3ee5759f3a3a2f1038a80937
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-04-30 08:46:50 -0700

    Add human_moves to ttt.rb

--- lesson_5/ttt.rb
+++ lesson_5/ttt.rb
@@ -1,50 +1,58 @@
 class Board
   INITIAL_MARKER = ' '
-  
+
   def initialize
     @squares = {}
     (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER) }
   end
-  
+
   def get_square_at(key)
     @squares[key]
+  end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
   end
 end
 
 class Square
+  attr_accessor :marker
+
   def initialize(marker)
     @marker = marker
   end
-  
+
   def to_s
     @marker
   end
 end
 
 class Player
-  def initialize
-  end
-  
-  def mark
+  attr_reader :marker
+
+  def initialize(marker)
+    @marker = marker
   end
 end
 
 class TTTGame
-  attr_reader :board
-  
+  attr_reader :board, :human, :computer
+
   def initialize
     @board = Board.new
+    @human = Player.new("X")
+    @computer = Player.new("O")
   end
-  
+
   def display_welcome_message
     puts "Welcome to Tic Tac Toe!"
     puts ""
   end
-  
+
   def display_goodbye_message
     puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
-  
+
   def display_board
     puts ""
     puts "     |     |"
@@ -60,16 +68,29 @@ class TTTGame
     puts "     |     |"
     puts ""
   end
-  
+
+  def human_moves
+    puts "Choose a square between 1-9: "
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if (1..9).include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+
+    board.set_square_at(square, human.marker)
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
+      human_moves
+      display_board
       break
-      first_player_moves
       break if someone_won? || board_full?
-      
-      second_player_moves
+
+      computer_moves
       break if someone_won? || board_full?
     end
     #display_result
