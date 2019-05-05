679304d2a2ef7152f9cafe25e2f83267b60d7b62
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-05-05 09:35:24 -0700

    Clarify relationship between Card and Deck classes

--- lesson_5/21.rb
+++ lesson_5/21.rb
@@ -1,7 +1,7 @@
 module Hand
   def busted?
   end
-  
+
   def total
     # we need to know about cards to produce some total
   end
@@ -9,7 +9,7 @@ end
 
 class Participant
   include Hand
-  
+
   def initialize
     @cards = []
     @total = 0
@@ -19,7 +19,7 @@ end
 class Player < Participant
   def hit
   end
-  
+
   def stay
   end
 end
@@ -27,37 +27,36 @@ end
 class Dealer < Participant
   def hit
   end
-  
+
   def stay
   end
 end
 
 class Deck
-  SUITS = %w(H D S C)
-  VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-  
-# I think it's a little weird to have a Deck object with a state of @deck. I have to either leave this or create another label. 
   def initialize
-    @deck = build_deck
+    @cards = build_deck
   end
-  
+
   def build_deck
-    combos = SUITS.product(VALUES)
-    combos.map! do |combo|
-      Card.new(combo)
+    combos = Card::SUITS.product(Card::VALUES)
+    combos.map! do |suit, value|
+      Card.new(suit, value)
     end
     combos.shuffle
   end
-  
+
   def deal
     # who deals - the dealer or the deck?
   end
 end
 
 class Card
-  # I think it's a litle weird to have a Card object with a state of @card
-  def initialize(suit_and_value_combo)
-    @card = suit_and_value_combo
+  SUITS = %w(H D S C)
+  VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
   end
 end
 
@@ -67,7 +66,7 @@ class Game
     @dealer = Dealer.new
     @deck = Deck.new
   end
-  
+
   def start
    #deal_cards
    #display_initial_cards
@@ -78,4 +77,3 @@ class Game
 end
 
 game = Game.new
-game.start
