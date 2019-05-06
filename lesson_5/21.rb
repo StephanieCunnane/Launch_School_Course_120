90b83c4aca0d77c8bd24bccbd882de430eebeffc
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-05-06 13:34:31 -0700

    Add scoring functionality

--- lesson_5/21.rb
+++ lesson_5/21.rb
@@ -40,11 +40,12 @@ end
 class Participant
   include Hand
 
-  attr_accessor :cards
+  attr_accessor :cards, :score
   attr_reader :referred_to_as
 
   def initialize
     @cards = []
+    @score = 0
   end
 end
 
@@ -176,6 +177,8 @@ class Card
 end
 
 class Game
+  WINNING_SCORE = 5
+
   attr_accessor :deck
   attr_reader :player, :dealer
 
@@ -189,13 +192,9 @@ class Game
     display_welcome_message
 
     loop do
-      shuffle_the_deck
-      deal_initial_cards
-      display_initial_cards
-      player.take_turn(deck)
-      dealer.take_turn(deck) unless player.busted?
-      display_result
-      break unless play_again?
+      play_round
+      display_game_result if someone_won?
+      break if someone_won? || !play_again?
       reset
     end
 
@@ -206,19 +205,42 @@ class Game
 
   def display_welcome_message
     puts '***************************************************'
+    puts ''
     puts 'Hello and welcome to 21!'
+    puts "The first player to #{WINNING_SCORE} points is the overall winner."
     puts "Let's see if you can beat the dealer!"
     puts 'Good luck!!'
+    puts ''
     puts '***************************************************'
     puts ''
-    sleep(1.25)
+    sleep(3.5)
+  end
+
+  def setup_round
+    shuffle_the_deck
+    deal_initial_cards
+    display_score
+    display_initial_cards
+  end
+
+  def add_point
+    player.score += 1 if [:player, :dealer_busted].include?(round_result)
+    dealer.score += 1 if [:dealer, :player_busted].include?(round_result)
+  end
+
+  def play_round
+    setup_round
+    player.take_turn(deck)
+    dealer.take_turn(deck) unless player.busted?
+    add_point
+    display_round_result
   end
 
   def display_spinner
     5.times do
       ["-", "\\", "|", "/"].each do |symbol|
         print symbol
-        sleep(0.1)
+        sleep(0.065)
         print "\b"
       end
     end
@@ -247,31 +269,60 @@ class Game
     end
   end
 
+  def display_score
+    clear_screen
+    puts '##### Current score #####'
+    puts "You: #{player.score}"
+    puts "Dealer: #{dealer.score}"
+    puts '#########################'
+    puts ''
+  end
+
   def display_initial_cards
     player.display_hand
     dealer.display_initial_hand
   end
 
-  def player_final_score
+  def player_round_score
     player.total
   end
 
-  def dealer_final_score
+  def dealer_round_score
     dealer.total
   end
 
-  def display_result
+  def round_result
     if player.busted?
-      puts 'Player busted -> Dealer wins!'
+      :player_busted
     elsif dealer.busted?
-      puts 'Dealer busted -> Player wins!'
-    elsif player_final_score > dealer_final_score
-      puts 'Player is the winner!!'
-    elsif dealer_final_score > player_final_score
-      puts 'Dealer is the winner!!'
+      :dealer_busted
+    elsif player_round_score > dealer_round_score
+      :player
+    elsif dealer_round_score > player_round_score
+      :dealer
     else
-      puts "It's a tie!"
+      :tie
     end
+  end
+
+  def display_round_result
+    result = round_result
+
+    case result
+    when :player_busted then puts 'Player busted -> Dealer wins!'
+    when :dealer_busted then puts 'Dealer busted -> Player wins!'
+    when :player then puts 'Player is the winner!!'
+    when :dealer then puts 'Dealer is the winner!!'
+    else puts "It's a tie!"
+    end
+  end
+
+  def someone_won?
+    player.score == WINNING_SCORE || dealer.score == WINNING_SCORE
+  end
+
+  def display_game_result
+    puts "And we have a winner!"
   end
 
   def play_again?
