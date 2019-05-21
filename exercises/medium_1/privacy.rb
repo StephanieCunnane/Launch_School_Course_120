3c58f313bc221e41cef9ddbaf7fc4aae064d00a2
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-05-21 06:34:07 -0700

    Modify Machine#status method

--- exercises/medium_1/privacy.rb
+++ exercises/medium_1/privacy.rb
@@ -25,3 +25,29 @@ class Machine
 end
 
 machine = Machine.new
+
+# Improved version
+
+class Machine
+  def start
+    flip_switch(:on)
+  end
+
+  def stop
+    flip_switch(:off)
+  end
+
+  def status
+    puts "Current machine status: #{switch == :on ? "On" : "Off"}"
+  end
+
+  private
+
+  attr_accessor :switch
+
+  def flip_switch(desired_state)
+    self.switch = desired_state
+  end
+end
+
+machine = Machine.new
